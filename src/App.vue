f916b8abc9e91e2e608875fad046302b8594a4c4
artem <artemfr@hotmail.com>
2023-10-02 13:12:45 +0200

    Added a subtitle

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <HelloWorld/>
+  <HelloWorld :sub-title="subTitle" />
+
+<input type="text" v-model="subTitle">
 </template>
 
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue';
 
+const subTitle = "Lorem ipsum";
 </script>
 
 <style scoped>
