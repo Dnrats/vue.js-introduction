40461a56b69a3d95c6a34e9da92bb4a582d05bb7
artem <artemfr@hotmail.com>
2023-10-02 13:37:37 +0200

    Added user info

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <HelloWorld :sub-title="subTitle" />
+  <HelloWorld :name="name"  :age = "age"/>
 
-<input type="text" v-model="subTitle">
+<input type="text" v-model="user.name">
+<input type="text" v-model="user.age">
 </template>
 
 <script setup>
 import HelloWorld from '@/components/HelloWorld.vue';
 
-const subTitle = "Lorem ipsum";
+import {reactive} from 'vue';
+
+const user = reactive ( target, {
+name: 'NordCoders',
+age: 30,
+});
+
+const display = () =>{
+  console.log(user.name, user.age);
+}
+
+display();
 </script>
 
 <style scoped>
